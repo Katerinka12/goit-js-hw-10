2b4dbabc88486a5ab73fddcb00a33f6acd8bc6ab
Katerinka12 <katerinka.kk12@gmail.com>
2022-10-20 14:39:50 +0300

    add idea

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import Notiflix from 'notiflix';
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
 // import { fetchCountries } from './js/fetchCountries.js';
@@ -9,3 +10,44 @@ const input = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
 const info = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
+
+input.addEventListener('input', _.debounce(e => {const trimInput = input.value.trim();
+    cleanHTML();
+    if(trimInput !== ' '){
+        fetchCountries(trimInput).then(foundData => {
+            if(foundData.length > 10){
+                Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+            }
+        }
+        else if(foundData.length === 0){
+            Notiflix.Notify.failure("Oops, there is no country with that name");
+        }
+        else if (foundData.length >= 2 $$ foundData.length <= 10){
+            renderCountryList(foundData);
+        }
+        else if (foundData.length === 1){
+            renderOneCountry(foundData);
+        });
+       
+    }
+    } , DEBOUNCE_DELAY))
+
+    function renderOneCountry(countries){
+        const markup = countries.map(country => {
+            return `<li> <img src = "${country.flag.svg}" alt = "Flag country ${country.name.official}" width = "30" hight = "20"
+            <b>$country.name.official</b>
+            <p><b>Capital:</b> ${country.capital}</p>
+            <p><b>Population:</b> ${country.population}</p>
+            <p><b>Languages:</b> ${country.languages}</p> </li>`}).join(" ");
+            list.innerHTML = markup;}
+        
+    function renderCountryList(countries){
+        const markup = countries.map(country => {
+            return `<li> <img src = "${country.flag.svg}" alt = "Flag country ${country.name.official}" width = "30" hight = "20"
+            <b>$country.name.official</b></li>`}).join('');
+            list.innerHTML = markup;
+    }
+ function cleanHTML(){
+    list.innerHTML = '';
+    info.innerHTML = '';
+ }
